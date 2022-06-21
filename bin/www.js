045133abd61d5dab5c7744301cb949e211b2a999
pchso1215@gmail.com <you@example.com>
2022-06-21 14:31:23 +0900

    port num

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,6 @@
 var app = require('../app');
-<<<<<<< HEAD
 var port = process.env.PORT ||4000;
-=======
-var port =process.env.PORT || 4000;
->>>>>>> ba322446dd02d422906524b9542dbf08441ebad0
+
 
 
 
